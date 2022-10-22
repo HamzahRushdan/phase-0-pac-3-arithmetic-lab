7b2f16b9994c7bc96df1e7eb997a252b8d2b6568
Hamzah Rushdan <hamzahrushdan79@gmail.com>
2022-10-22 11:13:27 -0400

    commit tree 85d4c5c19f1c98bc21f348661a3453cbaf473090

---
+++ index.js
@@ -0,0 +1,41 @@
+function add(a,b) {
+return a + b;
+}
+
+function subtract(a,b){
+return a - b;
+}
+
+function multiply(a, b) {
+
+    return a * b
+}
+
+function divide(a,b) {
+return a / b
+}
+
+
+function increment(n){
+    n++;
+    return n;
+}
+console.log(increment(721))
+
+function decrement(n){
+    --n;
+    return n;
+}
+
+function makeInt(n){
+    return parseInt(n, "10.00");
+    return makeInt(n);
+console.log(parseInt(n));  
+}
+makeInt(n);
+
+function preserveDecimal(n){
+    return parseFloat(n);
+}
+ preserveDecimal("80.12399");
+
